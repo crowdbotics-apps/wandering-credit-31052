f363efd3eec03338458ebc271a0bb37b0704e035
Crowdbotics <team@crowdbotics.com>
2021-10-02 19:24:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wandering_credit_31052",
+  url: "https://wandering_credit_31052.botics.co",
+  api: "https://wandering_credit_31052.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
